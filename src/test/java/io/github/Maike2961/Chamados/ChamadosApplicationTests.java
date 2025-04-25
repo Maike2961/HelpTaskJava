41493f27474dc4983efebfdfac7b83f182e4107b
Maike2961 <mayki.silva@hotmail.com>
2025-04-24 22:27:11 -0300

    feat(test): add tests with h2Database

---
+++ src/test/java/io/github/Maike2961/Chamados/ChamadosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Maike2961.Chamados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
